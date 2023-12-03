125defc88e15d9468ad13e68fe6d11382325ce95
Anna <a.arlanova@yandex.ru>
2023-12-04 03:40:08 +0500

    initial changes

---
+++ Programming-Language-Labs-CSharp/StringMaker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Language_Labs_CSharp
+{
+    internal class StringMaker
+    {
+        private string baseField;
+
+        // Конструктор по умолчанию
+        public StringMaker()
+        {
+            baseField = "Default";
+        }
+
+        // Конструктор копирования
+        public StringMaker(StringMaker other)
+        {
+            baseField = other.baseField;
+        }
+
+        // Метод для добавления трех восклицательных знаков в начало строки
+        public void AddThreeExclamation()
+        {
+            baseField = "!!!" + baseField;
+        }
+
+        // Перегрузка метода ToString
+        public override string ToString()
+        {
+            return $"Name: {baseField}";
+        }
+    } //password
+}
