9c32d34b1b3348111448f3889a07947d7b62cd14
Anna <a.arlanova@yandex.ru>
2023-12-05 02:53:56 +0500

    add new class with check correct input

--- Programming-Language-Labs-CSharp/Program.cs
+++ Programming-Language-Labs-CSharp/Program.cs
@@ -9,65 +9,6 @@ internal class Program
 {
     internal static void Main(string[] args)
     {
-        uint ReadIntFromUser(string msg)
-        {
-            Console.WriteLine($"{msg}");
-            uint result;
-
-            do
-            {
-                Console.Write("   Введите положительное число: ");
-                string input = Console.ReadLine();
-
-                // Пытаемся преобразовать введенное значение в int
-                bool isParsed = uint.TryParse(input, out result);
-
-                // Проверяем, что введено положительное число
-                if (isParsed && result > 0)
-                {
-                    // Введено корректное положительное число
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("   Ошибка! Введите корректное значение [1; 4294967295]");
-                }
-            } while (true);
-
-
-            Console.WriteLine();
-            return result;
-        }
-
-        byte ReadByteFromUser(string msg)
-        {
-            Console.WriteLine($"{msg}");
-            byte result;
-
-            do
-            {
-                Console.Write("   Введите положительное число: ");
-                string input = Console.ReadLine();
-
-                // Пытаемся преобразовать введенное значение в byte
-                bool isParsed = byte.TryParse(input, out result);
-
-                // Проверяем, что введено положительное число от 0 до 255
-                if (isParsed)
-                {
-                    // Введено корректное значение для byte
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("   Ошибка! Введите корректное значение [1; 255]");
-                }
-            } while (true);
-
-            Console.WriteLine();
-            return result;
-        }
-
         //Console.Write("Вызов Money(): ");
         //Money money1 = new Money();
         //Console.Write(money1.ToString());//выводится объект класса Money
@@ -78,14 +19,14 @@ internal class Program
         //Console.WriteLine();
 
         
-        uint user_rub = ReadIntFromUser("Ввод рублей");
+        uint user_rub = ReadFromUser.UInt("Ввод рублей");
 
-        byte user_kop = ReadByteFromUser("Ввод копеек");
+        byte user_kop = ReadFromUser.Byte("Ввод копеек");
 
         Money user_money = new Money(user_rub, user_kop);
         Console.WriteLine($"Переменная user_money = {user_money.ToString()}");
 
-        uint user_add_kop = ReadIntFromUser("Добавление копеек");
+        uint user_add_kop = ReadFromUser.UInt("Добавление копеек");
 
         user_money.AddKopeeks(user_add_kop);
 
---
+++ Programming-Language-Labs-CSharp/ReadFromUser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Language_Labs_CSharp
+{
+    internal class ReadFromUser
+    {
+        public static uint UInt(string msg)
+        {
+            Console.WriteLine($"{msg}");
+            uint result;
+
+            do
+            {
+                Console.Write("   Введите положительное число: ");
+                string input = Console.ReadLine();
+
+                // Пытаемся преобразовать введенное значение в int
+                bool isParsed = uint.TryParse(input, out result);
+
+                // Проверяем, что введено положительное число
+                if (isParsed && result > 0)
+                {
+                    // Введено корректное положительное число
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("   Ошибка! Введите корректное значение [1; 4294967295]");
+                }
+            } while (true);
+
+
+            Console.WriteLine();
+            return result;
+        }
+
+        public static byte Byte(string msg)
+        {
+            Console.WriteLine($"{msg}");
+            byte result;
+
+            do
+            {
+                Console.Write("   Введите положительное число: ");
+                string input = Console.ReadLine();
+
+                // Пытаемся преобразовать введенное значение в byte
+                bool isParsed = byte.TryParse(input, out result);
+
+                // Проверяем, что введено положительное число от 0 до 255
+                if (isParsed)
+                {
+                    // Введено корректное значение для byte
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("   Ошибка! Введите корректное значение [1; 255]");
+                }
+            } while (true);
+
+            Console.WriteLine();
+            return result;
+        }
+
+    }
+}
