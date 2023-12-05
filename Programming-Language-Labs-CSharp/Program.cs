2d1d83a843d225d1cbc0cf3215d35198c130523c
Anna Arlanova Laptop <a.arlanova@yandex.ru>
2023-12-05 08:30:00 +0500

    ппп

--- Programming-Language-Labs-CSharp/Program.cs
+++ Programming-Language-Labs-CSharp/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Programming_Language_Labs_CSharp;
+
+internal class Program
+{
+    internal static void Main(string[] args)
+    {
+        // 1 задние
+        // Бинарные файлы, содержащие числовые данные (исходный файл заполнить случайными данными, заполнение организовать отдельным методом)
+        // Вычислить произведение нечетных отрицательных компонент файла.
+    }
+}
