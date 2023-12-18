0d7da7f4a1429bb61fd36ab2549c738245f2b07e
Anna <a.arlanova@yandex.ru>
2023-12-19 03:15:41 +0500

    1st task

--- Programming-Language-Labs-CSharp/Program.cs
+++ Programming-Language-Labs-CSharp/Program.cs
@@ -2,15 +2,95 @@
 
 internal class Program
 {
+    private const string filePath = "task.bin";
+
+    static void CreateBinaryFile()
+    {
+        // Создаем и открываем файловый поток для записи в бинарный файл
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+        {
+            Console.WriteLine("Бинарный файл создан");
+        }
+
+    }
+
+    static void FillBinaryFile(int quantity, int diapozon)
+    {
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Append))
+        {
+            // Создаем объект BinaryWriter для записи данных в бинарный файл
+            using (BinaryWriter writer = new BinaryWriter(fileStream))
+            {
+                // Записываем рандомные значения в файл
+                Random random = new Random();
+                for (int i = 0; i < quantity; i++)
+                {
+                    int randomNumber = random.Next(-diapozon, diapozon);
+                    writer.Write(randomNumber);
+                }
+            }
+        }
+    }
+
+    static void OutputBinaryFile()
+    {
+        // Проверяем, существует ли файл
+        if (File.Exists(filePath))
+        {
+            // Создаем и открываем файловый поток для чтения из бинарного файла
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                // Создаем объект BinaryReader для чтения данных из бинарного файла
+                using (BinaryReader reader = new BinaryReader(fileStream))
+                {
+                    // Читаем данные из файла
+                    while (fileStream.Position < fileStream.Length)
+                    {
+                        int value = reader.ReadInt32();
+                        Console.Write($"{value} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Чтение бинарного файла завершено.");
+        }
+        else
+        {
+            Console.WriteLine("Файл не существует.");
+        }
+    }
+
+    static void CalculateProductOfNegativeOddNumbers()
+    {
+        using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+        {
+            int product = 1;
+            while (reader.BaseStream.Position < reader.BaseStream.Length)
+            {
+                int value = reader.ReadInt32();
+                if (value < 0 && value % 2 != 0)
+                {
+                    product *= value;
+                }
+            }
+            Console.WriteLine("Произведение нечетных отрицательных чисел из файла: " + product);
+        }
+    }
+
+
+
+
+
     internal static void Main(string[] args)
     {
         // 1 задние
         // Бинарные файлы, содержащие числовые данные (исходный файл заполнить случайными данными, заполнение организовать отдельным методом)
         // Вычислить произведение нечетных отрицательных компонент файла.
-        FileFuncion.CreateBinaryFile("task1.bin");
-        FileFuncion.FillBinaryFile("task1.bin", 10, 1000);
-        FileFuncion.OutputBinaryFile("task1.bin");
-        Console.WriteLine($"Произведение нечетных отрицательных компонент файла: %i", FileFuncion.MultiplyOddMinus("task1.bin"));
+        CreateBinaryFile();
+        FillBinaryFile(10, 1000);
+        OutputBinaryFile();
+        CalculateProductOfNegativeOddNumbers();
 
         // 2 задание
         // Бинарные файлы, содержащие числовые данные (исходный файл заполнить случайными данными, заполнение организовать отдельным методом)
