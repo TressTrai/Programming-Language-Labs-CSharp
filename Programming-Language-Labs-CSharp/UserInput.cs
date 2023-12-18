f364d5e25ff8102b68d5b55feb8f68dbfe93b3b7
Anna <a.arlanova@yandex.ru>
2023-12-19 02:26:45 +0500

    add checking class

---
+++ Programming-Language-Labs-CSharp/UserInput.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Language_Labs_CSharp
+{
+    internal class ReadFromUser
+    {
+        public static uint UInt(string msg)
+        {
+            Console.WriteLine($"{msg}");
+            uint result;
+
+            do
+            {
+                Console.Write("   Введите положительное число: ");
+                string input = Console.ReadLine();
+
+                // Пытаемся преобразовать введенное значение в int
+                bool isParsed = uint.TryParse(input, out result);
+
+                // Проверяем, что введено положительное число
+                if (isParsed)
+                {
+                    // Введено корректное положительное число
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("   Ошибка! Введите корректное значение [0; 4294967295]");
+                }
+            } while (true);
+
+            return result;
+        }
+
+        public static byte Byte(string msg)
+        {
+            Console.WriteLine($"{msg}");
+            byte result;
+
+            do
+            {
+                Console.Write("   Введите положительное число: ");
+                string input = Console.ReadLine();
+
+                // Пытаемся преобразовать введенное значение в byte
+                bool isParsed = byte.TryParse(input, out result);
+
+                // Проверяем, что введено положительное число от 0 до 255
+                if (isParsed)
+                {
+                    // Введено корректное значение для byte
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("   Ошибка! Введите корректное значение [0; 255]");
+                }
+            } while (true);
+
+            return result;
+        }
+
+    }
+}
